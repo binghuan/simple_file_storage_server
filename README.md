128ddb5d4016c0b6eadfd92b34d2d69c705da9d4
BH_Lin <binghuanlin@gmail.com>
2021-11-27 12:39:42 +0800

    first checked-in

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# simple_file_storage_server
-This is a simple file storage server.  User can upload file,  delete file and list file on the server. 
+![](./frontend/images/icon-256.jpg)  
+gomobile-bind-example
+================================================
+
+This is a simple file storage server.   
+User can upload file,  delete file and list file on the server. 
+
+If you want to build and run the server locally, here is the user guide. 
+
+## On Server Side
+### Prepare your Go Environment
+Download and install  
+<https://go.dev/doc/install>  
+
+### Start Backend Server
+Start the go server and listen the port 8080
+```shell
+cd backend;
+go run main.go
+```
+
+## On FrontEnd Side
+### Start Web Server to interact with server 
+You can use your favorite http-server.
+(in this case, I use this https://formulae.brew.sh/formula/http-server)
+```
+cd frontend;
+http-server --cors;
+```
+After launching the web server, you can visit the website. 
+![](./webpage.png)  
+Now, you can choose file to upload.  
+
+Once you have uploaded a file (ex: mylogo.png), 
+you can see there will be a file on the list. 
+![](./webpage_list.png)
+
+## Demo 
+![](./demo_20211127.gif)
+
+## How to Use CLI for Testing. 
+- Command to Upload file to server 
+```shell
+curl -v -F filename=image.jpg \
+    -F file2upload=@mylogo.png \
+    http://localhost:8080/files/mylogo.png
+```
+- Command to list files on server
+```shell
+curl --location --request GET 'http://localhost:8080/files'
+## Sample of output 
+## {"files":["mylogo.png"]}
+```
+- Command to delete files on server 
+```shell
+curl --location --request DELETE 'http://localhost:8080/mylogo.png'
+```
+
+
+
+
---
+++ frontend/index.js
@@ -0,0 +1,98 @@
+console.log("+++ index.js +++");
+
+let file2upload = null;
+let itemListView = null;
+
+const btnUpload = document.getElementById("btn_upload");
+if (btnUpload != null) {
+    btnUpload.onclick = () => {
+        console.log("Click button to upload file");
+        uploadFileToServer(file2upload);
+    }
+}
+
+function uploadFileToServer(file) {
+    console.log(">> uploadFileToServer");
+    let formData = new FormData();
+    formData.append("file2upload", file)
+
+    fetch('//localhost:8080/files/' + file.name, { // Your POST endpoint
+        method: 'POST',
+        body: formData // This is your file object
+    }
+    ).then(
+        success => {
+            console.log("OK>", success)
+            getFilesOnServer();
+        } // Handle the success response object
+    ).catch(
+        error => console.log("FAIL>", error) // Handle the error response object
+    );
+}
+
+function getFilesOnServer() {
+    console.log(">> getFilesOnServer");
+    fetch('//localhost:8080/files', {
+        method: 'GET'
+    }).then(respnose => respnose.json())
+        .then(respnose => {
+            console.log('Response', respnose)
+            showListItems(respnose.files)
+        }).catch(error => console.error('Error', error))
+}
+
+function deleteFileOnServer(fileName) {
+    console.log(">> deleteFileOnServer", fileName);
+    fetch('//localhost:8080/files/' + fileName, {
+        method: 'DELETE'
+    })
+        .then(respnose => {
+            console.log('Response', respnose)
+            getFilesOnServer();
+        }).catch(error => console.error('Error', error))
+}
+
+function initListItem() {
+    let listItems = document.querySelectorAll(".list-group-item");
+    itemListView = document.getElementById("item_list_view");
+    console.log(">> initListItem", listItems.length);
+    if (listItems.length > 0) {
+        defaultOfListItemView = listItems[0].cloneNode(true);
+
+        for (let i = 0; i < listItems.length; i++) {
+            console.log("Remove:");
+            itemListView.removeChild(listItems[i]);
+        }
+    }
+}
+
+function showListItems(files) {
+    console.log(">> showListItems", files);
+
+    initListItem();
+
+    let button2Delete = document.getElementById("button_delete_template");
+
+    for (let i = 0; i < files.length; i++) {
+        let fileName = files[i];
+        let listItem = defaultOfListItemView.cloneNode(true);
+        let button = button2Delete.cloneNode(true);
+        button.onclick = deleteFileOnServer.bind(this, fileName);
+        button.style.visibility = "visible";
+        button.innerText = "Delete";
+        console.log("List file:", fileName);
+        listItem.innerText = fileName;
+        listItem.style.visibility = "visible";
+        listItem.appendChild(button);
+        itemListView.appendChild(listItem);
+    }
+}
+
+const fileUploader = document.querySelector('#file-uploader');
+fileUploader.addEventListener('change', (e) => {
+    console.log("fileUploader.onChanged:", e.target.files); // get file object
+    file2upload = e.target.files[0];
+});
+
+initListItem();
+getFilesOnServer();
